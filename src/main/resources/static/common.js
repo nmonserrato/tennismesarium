1b6719255a07b1fb5fca598547408f572869b22a
neeno <neeno@nuc.dev>
2019-08-23 23:13:47 +0200

    new brackets view

---
+++ src/main/resources/static/common.js
@@ -0,0 +1,30 @@
+function urlParam(name) {
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
+}
+
+function submitMatchResult(matchId, winnerId, playerName) {
+    if(confirm("Do you confirm that "+playerName+" won the match?")) {
+        var tournamentId = document.getElementById("tournamentIdInput").value;
+        var postData = { "tournamentId" : tournamentId, "winnerId": winnerId}
+        $.ajax({
+            type: "PUT",
+            url: "/api/match/"+matchId,
+            data: JSON.stringify(postData),
+            complete: function (request, textStatus) {
+                reloadTree();
+            },
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            traditional: true
+        });
+    }
+}
+
+var alternativeView;
+if (window.location.href.indexOf("tree.html") !== -1) {
+    alternativeView = window.location.href.replace("tree.html", "brackets.html");
+} else {
+    alternativeView = window.location.href.replace("brackets.html", "tree.html");
+}
+$('<a href="'+alternativeView+'" style="position: absolute;right: 0; top: 0; display: block; margin: 20px 30px 0px 0px;">Change view</a>').appendTo(document.body);
